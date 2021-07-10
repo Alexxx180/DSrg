e40b6b46ab72bdb5dccc7cd6e137cc7331bcd0b0
Alexxx180 <67857746+Alexxx180@users.noreply.github.com>
2021-07-10 20:24:01 +0200

    Delete web site
    Deleted by security reasons

--- XEtremeTeam/Bodies/Scripts/DataBase.js
+++
@@ -1,7 +0,0 @@
-function Mode(Table) {
-document.getElementById(Table?"switcher-table":"switcher-form").style.display="grid";
-document.getElementById(Table?"switcher-form":"switcher-table").style.display="none";
-}
-function ChangeLocation(location){
-	window.location.replace(location);
-}
